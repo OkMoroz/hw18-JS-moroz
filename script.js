fe252b60c0a7583799c4cfb9de6b914d4a9f3bad
Oksana Moroz <k.moroz.mail@gmail.com>
2023-06-25 20:35:24 +0300

    dynamic icon

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const weatherBlock = document.querySelector("#weather");
-const xhr = new XMLHttpRequest();
 
 function loadWeather() {
   weatherBlock.innerHTML = `
@@ -10,7 +9,7 @@ function loadWeather() {
     </div>`;
 
   fetch(
-    "http://api.openweathermap.org/data/2.5/weather?q=Uzhgorod&units=metric&APPID=d7f1c188f618a6d306fef4f39faffce5"
+    "http://api.openweathermap.org/data/2.5/weather?q=Lviv&units=metric&APPID=d7f1c188f618a6d306fef4f39faffce5"
   )
     .then((response) => {
       if (response.ok) {
@@ -19,7 +18,6 @@ function loadWeather() {
         throw new Error("Response was not ok.");
       }
     })
-
     .then((weatherData) => {
       getWeather(weatherData);
     })
@@ -36,6 +34,7 @@ function getWeather(weatherData) {
   const humidity = Math.round(weatherData.main.humidity);
   const speed = Math.round(weatherData.wind.speed);
   const deg = Math.round(weatherData.wind.deg);
+  const icon = weatherData.weather[0].icon;
 
   const currentDateTime = new Date();
   const date = currentDateTime.toLocaleDateString();
@@ -46,14 +45,13 @@ function getWeather(weatherData) {
         <div class="weather-city">${location}</div>
         <div class="weather-temp">${temp}°C</div>
         <div class="weather-icon">
-            <img src="http://openweathermap.org/img/w/10d.png" alt="Icon 10d">
+            <img src="http://openweathermap.org/img/w/${icon}.png" alt="Icon">
         </div>
         <div class="weather-description">description: ${description}</div>
         <div class="weather-pressure">pressure: ${pressure} мм</div>
         <div class="weather-humidity">humidity: ${humidity} %</div>
         <div class="weather-speed">wind speed: ${speed} м/с</div>
          <div class="weather-deg">wind direction: ${deg}°</div>
-        
     </div>
 </div>
  <div class="weather-datetime">
